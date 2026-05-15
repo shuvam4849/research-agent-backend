ae03bb19563d242aca1dee08478cbed3faf60502
shubam <ssshuvam11@gmail.com>
2026-05-15 23:46:23 +0530

    research agentbackend4

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "https://your-vercel-app.vercel.app",
+        "https://vidhyaarthi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
